28464a0fea5ea10c86536c5c1cff884b7e4ab14a
rudyleiva93 <leivarudy93@gmail.com>
2023-12-23 20:08:43 -0800

    initial commit

---
+++ groomclient/Program.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Net.Client;
+using gRoom.gRPC.Messages;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5094/");
+var client = new Groom.GroomClient(channel);
+
+Console.Write("Eneter room name to register: ");
+var roomName = Console.ReadLine();
+var resp = client.RegisterToRoom(new RoomRegistrationRequest{RoomName = roomName});
+Console.WriteLine($"Room Id: {resp.RoomId}");
+
+Console.Read();
