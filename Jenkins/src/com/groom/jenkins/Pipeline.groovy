6caf9e3575daaf899d24655c3ec49eac1a576684
rudyleiva93 <leivarudy93@gmail.com>
2023-12-25 21:03:11 -0800

    initial Jenkinsfile push

---
+++ Jenkins/src/com/groom/jenkins/Pipeline.groovy
@@ -0,0 +1,8 @@
+package com.groom.jenkins
+
+import com.groom.jenkins.pipelines.GroomPipeline
+
+def groomPipeline() {
+    GroomPipeline pipeline = new GroomPipeline()
+    pipeline.build()
+}
---
+++ Jenkins/src/com/groom/jenkins/pipelines/GroomPipeline.groovy
@@ -0,0 +1,26 @@
+package com.groom.jenkins.pipelines
+
+def groomPipeline() {
+    pipeline {
+        options {
+            checkoutToSubdirectory('groom')
+            timestamps()
+        }
+        stages('Build gRoom Application') {
+            stage('Parallel Build') {
+                parallel {
+                    stage('Build gRoom Server'){
+                        dir('groomserver'){
+                            bat '"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" groomserver.sln -t:Build'
+                        }
+                    }
+                    stage('Build gRoom Client'){
+                        dir('groomserver'){
+                            bat '"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" groomclient.sln -t:Build'
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
