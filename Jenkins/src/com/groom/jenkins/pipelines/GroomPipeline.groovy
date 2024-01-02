4861160f1a3f55fec1ef619b48bd27c428462cdf
rudyleiva93 <leivarudy93@gmail.com>
2024-01-01 21:15:04 -0800

    Escaping backslashes

--- Jenkins/src/com/groom/jenkins/pipelines/GroomPipeline.groovy
+++ Jenkins/src/com/groom/jenkins/pipelines/GroomPipeline.groovy
@@ -11,12 +11,12 @@ def groomPipeline() {
                 parallel {
                     stage('Build gRoom Server'){
                         dir('groomserver'){
-                            bat '"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" groomserver.sln -t:Build'
+                            bat '"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" groomserver.sln -t:Build'
                         }
                     }
                     stage('Build gRoom Client'){
                         dir('groomserver'){
-                            bat '"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" groomclient.sln -t:Build'
+                            bat '"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" groomclient.sln -t:Build'
                         }
                     }
                 }
