9aa59a2a8997d2f1de528e9c7b7bff88535bd2c0
rudyleiva93 <leivarudy93@gmail.com>
2024-01-07 22:21:24 -0800

    Adding node syntax

--- Jenkins/src/com/groom/jenkins/pipelines/GroomPipeline.groovy
+++ Jenkins/src/com/groom/jenkins/pipelines/GroomPipeline.groovy
@@ -1,22 +1,24 @@
 package com.groom.jenkins.pipelines
 
 def build() {
-    pipeline {
-        options {
-            checkoutToSubdirectory('groom')
-            timestamps()
-        }
-        stages('Build gRoom Application') {
-            stage('Parallel Build') {
-                parallel {
-                    stage('Build gRoom Server'){
-                        dir('groomserver'){
-                            bat '"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" groomserver.sln -t:Build'
+    node {
+        pipeline {
+            options {
+                checkoutToSubdirectory('groom')
+                timestamps()
+            }
+            stages('Build gRoom Application') {
+                stage('Parallel Build') {
+                    parallel {
+                        stage('Build gRoom Server'){
+                            dir('groomserver'){
+                                bat '"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" groomserver.sln -t:Build'
+                            }
                         }
-                    }
-                    stage('Build gRoom Client'){
-                        dir('groomserver'){
-                            bat '"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" groomclient.sln -t:Build'
+                        stage('Build gRoom Client'){
+                            dir('groomserver'){
+                                bat '"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" groomclient.sln -t:Build'
+                            }
                         }
                     }
                 }
