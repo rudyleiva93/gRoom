f13a1f99413710f10cd7eb68021f3fd5a2aa3c28
rudyleiva93 <leivarudy93@gmail.com>
2024-01-13 10:58:58 -0800

    Remove node syntax

--- Jenkins/src/com/groom/jenkins/pipelines/GroomPipeline.groovy
+++ Jenkins/src/com/groom/jenkins/pipelines/GroomPipeline.groovy
@@ -1,22 +1,22 @@
 package com.groom.jenkins.pipelines
 
 def build() {
-    node {
-        pipeline {
+    pipeline {
+        options {
             checkoutToSubdirectory('groom')
-            //timestamps()
-            stages('Build gRoom Application') {
-                stage('Parallel Build') {
-                    parallel {
-                        stage('Build gRoom Server'){
-                            dir('groomserver'){
-                                bat '"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" groomserver.sln -t:Build'
-                            }
+            timestamps()
+        }
+        stages('Build gRoom Application') {
+            stage('Parallel Build') {
+                parallel {
+                    stage('Build gRoom Server') {
+                        dir('groomserver') {
+                            bat '"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" groomserver.sln -t:Build'
                         }
-                        stage('Build gRoom Client'){
-                            dir('groomserver'){
-                                bat '"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" groomclient.sln -t:Build'
-                            }
+                    }
+                    stage('Build gRoom Client') {
+                        dir('groomclient') {
+                            bat '"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" groomclient.sln -t:Build'
                         }
                     }
                 }
