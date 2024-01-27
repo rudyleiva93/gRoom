55f23ac535b9b3edb85f3f836511c83385caa89d
rudyleiva93 <leivarudy93@gmail.com>
2024-01-27 08:07:17 -0800

    Add restore option

--- Jenkins/vars/groomPipeline.groovy
+++ Jenkins/vars/groomPipeline.groovy
@@ -11,14 +11,14 @@ def call() {
                     stage('Build gRoom Server') {
                         steps {
                             dir('groom\\groomserver') {
-                                bat '"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" groomserver.sln -t:Build'
+                                bat '"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" groomserver.sln -restore -t:Build'
                             }
                         }
                     }
                     stage('Build gRoom Client') {
                         steps {
                             dir('groom\\groomclient') {
-                                bat '"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" groomclient.sln -t:Build'
+                                bat '"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\MSBuild\\Current\\Bin\\MSBuild.exe" groomclient.sln -restore -t:Build'
                             }
                         }
                     }
